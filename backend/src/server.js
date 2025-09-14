1423439d1bc4491cb163b548a34841fe1ffd8ce1
iswarya-madamanchi <rr200703@rguktrkv.ac.in>
2025-09-14 21:57:54 +0530

    fixed code

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,9 +14,9 @@ const port = process.env.PORT || 5000
 const __dirname = path.resolve();
 
 if(process.env.NODE_ENV !== 'production'){
-    app.use(cors({
-      origin: "http://localhost:5173"
-    }));
+  app.use(cors({
+    origin: "http://localhost:5173"
+  }));
 }
 app.use(express.json()); //middleware
 app.use((req,res,next)=>{
@@ -29,7 +29,7 @@ app.use("/api/notes",notesRoutes)
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,'../frontend/dist')));
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'../frontend',"dist","index.html"))
+        res.sendFile(path.join(__dirname,'../frontend/dist/index.html'))
     })
 }
 
